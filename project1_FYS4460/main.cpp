70adeb60afc3b4bf5c5dab7c63fc27ae2830b0f8
goranbs <goranbs@student.matnat.uio.no>
2014-01-30 10:05:54 +0100

    still going strong

--- project1_FYS4460/main.cpp
+++ project1_FYS4460/main.cpp
@@ -27,6 +27,10 @@ int main(int argc, char* argv[]){
     // create a vector within a vector, using including the <vector> library.
     vector < vector < double > > atom (N_atoms*4, vector < double > (3,0));
 
+    /*
+     * Making the initial state:
+     */
+
     natom = 0;
     for (int ix=0; ix < Nx; ++ix) {
         for (int iy=0; iy < Ny; ++iy){
@@ -60,7 +64,7 @@ int main(int argc, char* argv[]){
     }
     cout << "-------------------------------------------" << endl;
 
-    //string filename = "state000.xyz", open file and write initial state;
+    //string filename = "state000.xyz", open file and write initial state to file;
     ofstream myfile;
     myfile.open("state000.xyz");
     myfile << N_atoms*4 << endl;
@@ -70,25 +74,39 @@ int main(int argc, char* argv[]){
     }
     myfile.close();
 
+
+
     /* Now we should give the particles some initial velocity, and then calculate the new positions
      * accordingly. What to do with the boundaries then?
      */
 
+    // r_ij is holding vectors of distances between every particle in the grid:
+    vector < vector < double > > r_ij (N_atoms*N_atoms*16, vector < double > (3,0));
+    // velocity should contain the velocity of every particle in the grid:
     vector < vector < double > > velocity (N_atoms*4, vector < double > (3,0) );
+
     double rx_ij,ry_ij,rz_ij, rx,ry,rz, r2,r2i,r6i,r12i;
-    vector < vector < double > > f_ij (N_atoms*Natoms*16, vector < double > (3,0));
+    vector < vector < double > > f_ij (N_atoms*N_atoms*16, vector < double > (3,0));
+    vector < vector < double > > r_ij ();
     int force = 0;
     for (int i = 0; i < N_atoms*4; ++i) {
         rx = atom[i][0]; ry = atom[i][1]; rz = atom[i][2];
         for (int j = 0; j < N_atoms*4; ++j) {
+            r_ij[force][0] = rx - atom[j][0];
+            r_ij[force][1] = ry - atom[j][1];
+            r_ij[force][2] = rz - atom[j][2];
+
             rx_ij =  rx - atom[j][0];ry_ij =  ry - atom[j][1];ry_ij =  rz - atom[j][2];
-            r2 = rx_ij*rx_ij + ry_ij*ry_ij + rz_ij*rz_ij;
+            r2 =rx_ij*rx_ij + ry_ij*ry_ij + rz_ij*rz_ij;
             r2i = 1.0/r2;
             r6i = r2i*r2i*r2i;
             r12i = r6*r6;
-            f_ij[force][0] = 24*(2*r12i - r6i)*r2i*rx;
-            f_ij[force][1] = 24*(2*r12i - r6i)*r2i*ry;
-            f_ij[force][2] = 24*(2*r12i - r6i)*r2i*rz;
+
+            //f_ij = 24*(2*r12i - r6i)*r2i*rx*r_ij;
+            f_ij[force][0] = 24*(2*r12i - r6i)*r2i*rx_ij;
+            f_ij[force][1] = 24*(2*r12i - r6i)*r2i*ry_ij;
+            f_ij[force][2] = 24*(2*r12i - r6i)*r2i*rz_ij;
+
             fi = fi + f_ij;
             fj = fj - f_ij;
 
