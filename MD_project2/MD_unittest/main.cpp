1c4a3ae414e6ec32cb23071e3786a05a39c1b040
goranbs <goranbs@student.matnat.uio.no>
2014-03-25 09:07:29 +0100

    unittests are working

--- MD_project2/MD_unittest/main.cpp
+++ MD_project2/MD_unittest/main.cpp
@@ -2,6 +2,7 @@
 #include <atom.h>
 #include <unitconverter.h>
 #include <iostream>
+#include <list>
 
 TEST(Atom) {
     Atom argon({1,2,3},{1,2,3},{0.1,0.1,0.1},0.0);
@@ -28,7 +29,7 @@ TEST(Atom2){
     vector <double> v2 (3,0.0);
     vector <double> f2 (3,0.0);
     bool grandtruth,R_truevalue,F_truevalue,V_truevalue, u_true;
-    grandtruth = true;
+    grandtruth = false;
     u_true = true;
     R_truevalue = true;
     V_truevalue = true;
@@ -54,7 +55,7 @@ TEST(Atom2){
             for (int k = 0; k < 3; ++k) {
                 r2[i][k] = 0.1*time;
                 v[i][k] = 0.5*time;
-                f[i][k] = 6*time;
+                f[i][k] = 7*time;
             }
             potential = u0*time;
             atoms[i].update_position(r[i]);
@@ -67,57 +68,84 @@ TEST(Atom2){
         r0[i] = atoms[i].return_initial_position();
     }
 
+    /*
     cout << "Before test: "<< endl;
     cout << "Grand truth= " << grandtruth << endl;
     cout << "R_truevalue= " << R_truevalue << endl;
     cout << "F_truevalue= " << F_truevalue << endl;
     cout << "V_truevalue= " << V_truevalue << endl;
     cout << "u_truevalue= " << u_true << endl;
+    */
 
     for (int i = 0; i < N; ++i) {
         for (int k = 0; k < 3; ++k) {
             if (r[i][k]!=r0[i][k]){
                 R_truevalue = false;
+                char r1_name [10];
+                char r0_name [10];
+                sprintf(r1_name, "r[%d][%d]",i,k);
+                sprintf(r0_name, "r0[%d][%d]",i,k);
+                cout << "r1[i][k] = " << r1_name << " = " << r[i][k] << endl;
+                cout << "r0[k] = " << r0_name << " = " << r0[i][k] << endl;
                 continue;
             }
-            f2 = atoms[i].position();
+            f2 = atoms[i].force();
             if (f[i][k] != f2[k]){
                 F_truevalue = false;
+                char f1_name [10];
+                char f2_name [10];
+                sprintf(f1_name, "f1[%d][%d]",i,k);
+                sprintf(f2_name, "f2[%d]",k);
+                cout << "f1[i][k] = " << f1_name << " = " << f[i][k] << endl;
+                cout << "f2[k] = " << f2_name << " = " << f2[k] << endl;
                 continue;
             }
             v2 = atoms[i].velocity();
             if (v[i][k] != v2[k]){
                 V_truevalue = false;
+                char v1_name [10];
+                char v2_name [10];
+                sprintf(v1_name, "v1[%d][%d]",i,k);
+                sprintf(v2_name, "v2[%d]",k);
+                cout << "v1[i][k] = " << v1_name << " = " << v[i][k] << endl;
+                cout << "v2[k] = " << v2_name << " = " << v2[k] << endl;
                 continue;
             }
 
-            if (atoms[i].potential() != u0*t_max){
+            if (atoms[i].potential() != u0*(t_max-1)){
                 u_true = false;
+                cout << "error in the update of the potential value" << endl;
+                cout << i << " u0*time = " << u0*t_max << " atoms[i].potential() = " << atoms[i].potential() << endl;
                 continue;
             }
         }
     }
+    /*
     cout << "After test: " << endl;
     cout << "R_truevalue= " << R_truevalue << endl;
     cout << "F_truevalue= " << F_truevalue << endl;
     cout << "V_truevalue= " << V_truevalue << endl;
     cout << "u_truevalue= " << u_true << endl;
-
+    */
     if (R_truevalue != false) {
         grandtruth = false;
+        //cout << "R is true" << endl;
         if (V_truevalue != false) {
             grandtruth = false;
+            //cout << "V is true" << endl;
             if (F_truevalue != false){
                 grandtruth = false;
+                //cout << "F is true" << endl;
                 if (u_true != false) {
-                    grandtruth = false;
+
+                    grandtruth = true;
 
                 }
             }
         }
     }
 
-    cout << "Grand truth= " << grandtruth << endl;
+    //cout << "Grand truth= " << grandtruth << endl;
     CHECK(grandtruth == true);
 }
 
@@ -126,8 +154,8 @@ TEST(ATOM_Force){
     vector <double> f2 (3,0.0);
     vector <double> f3 (3,0.0);
     bool unlike, like;
-    unlike = true;
-    like = true;
+    unlike = true;  // true
+    like = true;    // this should be false, f1 != f3
     for (int k = 0; k < 3; ++k) {
         f1[k] = 0.11;
         f2[k] = 0.12;
@@ -142,15 +170,31 @@ TEST(ATOM_Force){
     }
     //argon.reset_force();
     //f1 = argon.force();
+    /*
     for (int k = 0; k < 3; ++k) {
         cout << f1[k] << "== 0.11" << endl;
         cout << f2[k] << "== 0.12" << endl;
     }
+    */
 
     CHECK(unlike != like);
-
 }
 
+/*
+TEST(test2particles){
+    double L,T_bath;
+    int N;
+    vector < list < int > > box_list;
+
+    N = 3;
+    L = 1.0;
+    T_bath = 0.851;
+
+    test_2particles(L,L,L,N,N,N,box_list,T_bath);
+    cout << "test_2particles. See output file" << endl;
+    CHECK(2==2);
+}
+*/
 
 int main() {
 
